31f2347da8d0949d2b1385a742b01ce5588366be
stathismegas <stathismegas@gmail.com>
2024-07-25 16:01:57 +0000

    add helper functions

---
+++ utils/helpers.py
@@ -0,0 +1,44 @@
+# define a function to derive the gex from the sphex
+def calc_gex(sphex):
+    """
+    Calculates the gene expression matrix from the spherical
+    """
+    # setup the gex
+    n_genes = sphex.shape[1]+1
+    gex = torch.from_numpy(np.zeros((sphex.shape[0], n_genes)).astype('float32'))
+    # compute the gex
+    for idx in range(n_genes):
+        if idx == n_genes-1:
+            gex[:,idx] = torch.sin(sphex[:,idx-1])
+        else:
+            gex[:,idx] = torch.cos(sphex[:,idx])
+        for idx_ in range(idx):
+            gex[:,idx] *= torch.sin(sphex[:,idx_])
+    return gex
+
+# define a function to gather positions
+def get_pos(n_x, n_y):
+    # create the hex lattice
+    xs = np.array([np.arange(0, n_x) + 0.5 if idx % 2 == 0 else np.arange(0, n_x) for idx in range(n_y)])
+    # derive the y-step given a distance of one
+    y_step = np.sqrt(1**2+0.5**2)
+    ys = np.array([[y_step * idy] * n_x for idy in range(n_y)])
+    # define the positions
+    pos = np.vstack([xs.flatten(), ys.flatten()]).T
+    return pos
+
+
+# define a function to normalize the g2g
+def normalize_g2g(g2g):
+    """
+    Addresses any small fluctuations in symmetrical weights
+    """
+    # symmetrize the values
+    g2g = (g2g + g2g.T) / 2
+    # force them to be between 0-1
+    g2g[g2g < 0] = 0
+    g2g[g2g > 1] = 1
+    # force the central line to be 1
+    for idx in range(len(g2g)):
+        g2g[idx, idx] = 1
+    return g2g
