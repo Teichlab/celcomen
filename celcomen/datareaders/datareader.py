5021f87a5a5d6721a4b5393ec75ebfbee29760c0
stathismegas <stathismegas@gmail.com>
2024-08-30 12:55:21 +0100

    datareaders and loaders for model

---
+++ celcomen/datareaders/datareader.py
@@ -0,0 +1,42 @@
+from torch_geometric.loader import DataLoader
+import scanpy as sc
+import torch
+import torch_geometric
+#from torch_cluster.knn import knn_graph
+from sklearn.neighbors import kneighbors_graph
+
+def get_dataset_loaders(h5ad_path: str, sample_id_name: str, n_neighbors: int, verbose: bool):
+
+    adata = sc.read_h5ad(h5ad_path) 
+
+    adata_list = [  adata[adata.obs[sample_id_name]==i] for i in set(adata.obs[sample_id_name])  ]
+
+    data_list = []
+    n_neighbors = 6
+
+    for adata in adata_list:
+        pos = torch.from_numpy(adata.obsm["spatial"])
+        x = torch.from_numpy(adata.X.todense())    # here x is nodes x 33500 -> add filteration here to take "best" 100
+        # normalize x 
+        norm_factor = torch.pow(x,2).sum(1).reshape(-1,1)
+        x = torch.div(x, norm_factor)
+        y = torch.Tensor([0])   # here we will store GT value
+        #edge_index = knn_graph(pos, k=n_neighbors)
+        edge_index = kneighbors_graph(pos, n_neighbors, include_self=False).todense()
+        edge_index = torch.from_numpy(np.array(np.where(edge_index)))
+        data = torch_geometric.data.Data(x=x, pos=pos, y=y, edge_index=edge_index)
+        data.validate(raise_on_error=True)    # performs basic checks on the graph
+        data_list.append(data)
+
+    loader = DataLoader( data_list, batch_size=1, shuffle=True)
+
+    if verbose:
+        for step, data in enumerate(loader):
+            print(f'Step {step+1}')
+            print("=====")
+            print(f'Number of graphs in the current batch: {data.num_graphs}')
+            print(data)
+            print()
+
+    return loader
+                
