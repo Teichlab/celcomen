162d0453b9f68a2eaab0c5382d992bda79a0597d
stathismegas <stathismegas@gmail.com>
2024-09-02 14:45:01 +0100

    train from tutorial gpu

--- celcomen/training_plan/train.py
+++ celcomen/training_plan/train.py
@@ -2,29 +2,7 @@ from tqdm import tqdm
 import numpy as np
 import torch
 
-# def train(model, optimizer, train_loader, num_epochs, seed=1, device="cpu"):
-    
-#     if device is None:
-#         device = torch.device(device)
-    
-#     torch.manual_seed(seed)
-    
-#     for epoch in range(num_epochs):
-        
-#         model = model.train()
-#         for batch_idx, (features, labels) in enumerate(train_loader):
-            
-#             features, labels = features.to(device),  labels.to(device)
-            
-#             logits = model(features)
-            
-#             loss = F.cross_entropy(logits, labels)
-#             optimizer.zero_grad()
-#             loss.backward()
-#             optimizer.step()
-            
-            
-def train(num_epochs, learning_rate, model, loader, seed=1, device="cpu"):
+def train(num_epochs, learning_rate, model, loader, zmft_scalar=1e-1, seed=1, device="cpu"):
     
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0)
     losses = []
@@ -44,7 +22,10 @@ def train(num_epochs, learning_rate, model, loader, seed=1, device="cpu"):
             msg, msg_intra, log_z_mft = model(data.edge_index, 1)
             # compute the loss and track it
             loss = -(-log_z_mft + zmft_scalar * torch.trace(torch.mm(msg, torch.t(model.gex))) + zmft_scalar * torch.trace(torch.mm(msg_intra, torch.t(model.gex))) )
-            losses_.append(loss.detach().numpy()[0][0])
+            if device=="cpu":
+                losses_.append(loss.detach().numpy()[0][0])
+            else:
+                losses_.append(loss.detach().cpu().numpy()[0][0])
             # derive the gradients, update, and clear
             loss.backward()
             optimizer.step()
@@ -54,7 +35,7 @@ def train(num_epochs, learning_rate, model, loader, seed=1, device="cpu"):
             model.lin.weight = torch.nn.Parameter(normalize_g2g(model.lin.weight), requires_grad=True)
             optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0)
             
-        print(f"Loss={np.mean(losses_)}")
+        print(f"Epoch={epoch}   |   Loss={np.mean(losses_)}")
         losses.append(np.mean(losses_))
 
     return losses
