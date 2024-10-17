e0fefcb83efbe707e77198caa8dafc801fcb6d64
stathismegas <stathismegas@gmail.com>
2024-10-17 11:17:50 +0100

    add helpers inside simcomen

--- celcomen/models/simcomen.py
+++ celcomen/models/simcomen.py
@@ -2,7 +2,7 @@ from torch_geometric.nn import GCNConv
 from sklearn.neighbors import kneighbors_graph
 import torch
 import numpy as np
-from ..utils.helpers import calc_gex
+#from ..utils.helpers import calc_gex
 
 # define the number of neighbors (six for visium)
 n_neighbors = 6
@@ -98,3 +98,66 @@ class simcomen(torch.nn.Module):
         else:
             z_interaction = num_spots * torch.log((torch.exp( g) - torch.exp(- g))/( g))
         return z_interaction
+
+
+    # define a function to derive the gex from the sphex
+    def calc_gex(sphex):
+        """
+        Calculates the gene expression matrix from the spherical
+        """
+        # setup the gex
+        n_genes = sphex.shape[1]+1
+        gex = torch.from_numpy(np.zeros((sphex.shape[0], n_genes)).astype('float32'), device=next(model.parameters()).device)
+        # compute the gex
+        for idx in range(n_genes):
+            if idx == n_genes-1:
+                gex[:,idx] = torch.sin(sphex[:,idx-1])
+            else:
+                gex[:,idx] = torch.cos(sphex[:,idx])
+            for idx_ in range(idx):
+                gex[:,idx] *= torch.sin(sphex[:,idx_])
+        return torch.nan_to_num(gex)
+    
+    # define a function to gather positions
+    def get_pos(n_x, n_y):
+        # create the hex lattice
+        xs = np.array([np.arange(0, n_x) + 0.5 if idx % 2 == 0 else np.arange(0, n_x) for idx in range(n_y)])
+        # derive the y-step given a distance of one
+        y_step = np.sqrt(1**2+0.5**2)
+        ys = np.array([[y_step * idy] * n_x for idy in range(n_y)])
+        # define the positions
+        pos = np.vstack([xs.flatten(), ys.flatten()]).T
+        return pos
+    
+    
+    # define a function to normalize the g2g
+    def normalize_g2g(g2g):
+        """
+        Addresses any small fluctuations in symmetrical weights
+        """
+        # symmetrize the values
+        g2g = (g2g + g2g.T) / 2
+        # force them to be between 0-1
+        g2g[g2g < 0] = 0
+        g2g[g2g > 1] = 1
+        # force the central line to be 1
+        for idx in range(len(g2g)):
+            g2g[idx, idx] = 1
+        return g2g
+    
+    # define a function to derive the gex from the sphex
+    def calc_sphex(gex):
+        """
+        Calculates the spherical expression matrix from the normal
+        """
+        # setup the gex
+        n_sgenes = gex.shape[1]-1
+        sphex = torch.from_numpy(np.zeros((gex.shape[0], n_sgenes)).astype('float32'), device=next(model.parameters()).device)
+        # compute the gex
+        for idx in range(n_sgenes):
+            sphex[:,idx] = gex[:,idx]
+            for idx_ in range(idx):
+                sphex[:,idx] /= torch.sin(sphex[:,idx_])
+            sphex[:,idx] = torch.arccos(sphex[:,idx])
+        return sphex
+    
